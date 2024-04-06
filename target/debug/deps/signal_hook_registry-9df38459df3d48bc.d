df3fa4267573de2b1293621deae3653c4820ffbb
sergey2gnezdilov <sergey.n.gnezdilov@gmail.com>
2024-04-06 20:13:35 +0300

    sync

--- target/debug/deps/signal_hook_registry-9df38459df3d48bc.d
+++ target/debug/deps/signal_hook_registry-9df38459df3d48bc.d
@@ -1,8 +1,8 @@
-/home/sg/rust/urs_mng_api/target/debug/deps/libsignal_hook_registry-9df38459df3d48bc.rmeta: /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/lib.rs /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/half_lock.rs
+/home/sg/git_clone/RUST_API_REST_USER_MNG/target/debug/deps/libsignal_hook_registry-9df38459df3d48bc.rmeta: /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/lib.rs /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/half_lock.rs
 
-/home/sg/rust/urs_mng_api/target/debug/deps/libsignal_hook_registry-9df38459df3d48bc.rlib: /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/lib.rs /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/half_lock.rs
+/home/sg/git_clone/RUST_API_REST_USER_MNG/target/debug/deps/libsignal_hook_registry-9df38459df3d48bc.rlib: /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/lib.rs /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/half_lock.rs
 
-/home/sg/rust/urs_mng_api/target/debug/deps/signal_hook_registry-9df38459df3d48bc.d: /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/lib.rs /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/half_lock.rs
+/home/sg/git_clone/RUST_API_REST_USER_MNG/target/debug/deps/signal_hook_registry-9df38459df3d48bc.d: /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/lib.rs /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/half_lock.rs
 
 /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/lib.rs:
 /home/sg/.cargo/registry/src/index.crates.io-6f17d22bba15001f/signal-hook-registry-1.4.1/src/half_lock.rs:
